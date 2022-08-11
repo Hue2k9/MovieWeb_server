438b8a2bfd9108902d53e29ff624d415f16cdd95
Hoàng Minh Huệ <httt2hauik15@gmail.com>
2022-08-11 23:52:12 +0700

    movie web

---
+++ models/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  User: require("./user.model"),
+  Token: require("./token.model"),
+  Movie: require("./movie.model"),
+  Comment: require("./comment.model"),
+  Video: require("./video.model"),
+  History: require("./history.model"),
+};
---
+++ routes/movie.route.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const movieRouter = express.Router();
+const { movieController } = require("../controllers");
+const auth = require("../middlewares/auth");
+
+movieRouter.route("/videos").get(movieController.getVideos);
+movieRouter.route("/videos/:movieId").get(movieController.getVideosById);
+
+//auth
+movieRouter
+  .route("/")
+  .get(movieController.getMovies)
+  .post(movieController.createMovie);
+
+movieRouter.route("/:movieId").get(movieController.getMovieById);
+
+movieRouter.route("/video/:movieId/:episode").get(movieController.getVideo);
+
+movieRouter.route("/:movieId/evaluate").put(movieController.evaluateMovie);
+
+module.exports = movieRouter;
